4117b310057426434bb090c2a4149ee85eff43f9
Ignacio Rubio Paredes <ignacio@MacBook-Pro-de-Ignacio.local>
2020-09-06 17:36:48 +0200

    Created requirements.txt file for dependencies

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from flask_restful import Api
 
+from src.resources.country import Country, CountryList
+from src.resources.measure import Measure, MeasureList
+from src.resources.record import Record, RecordList
+
 app = Flask(__name__)
 api = Api(app)
 
 # all endpoints
+api.add_resource
